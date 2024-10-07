efd105109fb3e524e7788de1cd461dd8ac1095c7
HamzaMerdassi55 <hamzamerdassi41@gmail.com>
2024-10-07 20:20:13 +0100

    model_c

--- model.go
+++ model.go
@@ -6,3 +6,22 @@ type product struct {
 	Quantity int `json:"quantity"`
 	Price float64 `json:"price"`
 }
+
+func getProducts(db *sql.DB) ([]product, error) {
+	query:= "SELECT id , name , quantity, price from products"
+	rows, err := db.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	products := []product{}
+	for rows.Next() {
+		var p product
+        err := rows.Scan(&p.ID, &p.Name, &p.Quantity, &p.Price)
+        if err!= nil 
+        products = append(products, p)
+	}
+	
+	return products, nil
+}
